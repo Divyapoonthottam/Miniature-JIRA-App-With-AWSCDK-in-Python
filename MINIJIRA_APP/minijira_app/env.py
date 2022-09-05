7bdbc8e414276092c47b858311754990429764d0
Divya Poonthottam <96609766+Divyapoonthottam@users.noreply.github.com>
2022-09-05 11:19:17 +0530

    first commit

---
+++ MINIJIRA_APP/minijira_app/env.py
@@ -0,0 +1,7 @@
+
+# here region is your default region
+# aws -account id is the account used to verify these email ids . your account id 
+# email id are verifed ids 
+
+mail_list =['arn:aws:ses:<region>:<aws account id>:identity/<email id 1>','arn:aws:ses:<region>:<aws account id >:identity/<email id 2>','arn:aws:ses:<region>:<aws account id>:identity/<email id 3>',]
+        
